11d4637e5df6636dfe2fa8b18cd5e673bd7e0a68
nikethaapa@gmail.com <nikethaapa@gmail.com>
2026-03-15 12:50:39 +0530

    msg

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+
+var menu = document.getElementById("menu")
+
+function openMenu()
+{
+    menu.style.left = "0"
+}
+
+function closeMenu()
+{
+    menu.style.left = "-300px"
+}
+
+</script>
